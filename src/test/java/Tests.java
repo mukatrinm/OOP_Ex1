e2de6f8bae329cc55a67e9be686a38a545825a69
AmitRazi <115696533+AmitRazi@users.noreply.github.com>
2022-12-29 18:01:19 +0200

    Update Tests.java
    Added logger tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,21 +1,50 @@
+import observer.ConcreteMember;
+import observer.GroupAdmin;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
 public class Tests {
     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
+    GroupAdmin ga1, ga2;
+    ConcreteMember member1, member2, member3;
+
+    @BeforeEach
+    void setUp() {
+        ga1 = new GroupAdmin();
+        member1 = new ConcreteMember();
+        member2 = new ConcreteMember();
+        member3 = new ConcreteMember();
+    }
+
     // stub method to check external dependencies compatibility
     @Test
     public void test(){
-        String s1 = "Alice";
-        String s2 = "Bob";
-
-        logger.info(()->JvmUtilities.objectFootprint(s1));
-
-        logger.info(()->JvmUtilities.objectFootprint(s1,s2));
-
-        logger.info(()->JvmUtilities.objectTotalSize(s1));
-
-        logger.info(() -> JvmUtilities.jvmInfo());
+        logger.info(()->JvmUtilities.objectFootprint(ga1));
+        ga1.register(member1);
+        ga1.register(member2);
+        logger.info(()->JvmUtilities.objectFootprint(ga1));
+        logger.info(()->"member 1 before insert");
+        logger.info(()->JvmUtilities.objectFootprint(member1));
+        ga1.insert(0, "abc");
+        logger.info(()->"ga1 and all members after insert");
+        logger.info(()->JvmUtilities.objectFootprint(ga1));
+        logger.info(()->JvmUtilities.objectFootprint(member1));
+        logger.info(()->JvmUtilities.objectFootprint(member2));
+        logger.info(()->JvmUtilities.objectFootprint(member3));
+        ga1.insert(0, "def");
+        logger.info(()->"ga1 and all members after second insert");
+        logger.info(()->JvmUtilities.objectFootprint(ga1));
+        logger.info(()->JvmUtilities.objectFootprint(member1));
+        logger.info(()->JvmUtilities.objectFootprint(member2));
+        logger.info(()->JvmUtilities.objectFootprint(member3));
+        ga1.undo();
+        logger.info(()->JvmUtilities.objectFootprint(ga1));
+        logger.info(()->JvmUtilities.objectFootprint(member1));
+        logger.info(()->JvmUtilities.objectFootprint(member2));
+        logger.info(()->JvmUtilities.objectFootprint(member3));
+        logger.info(()->JvmUtilities.objectTotalSize(ga1));
+        logger.info(()->JvmUtilities.jvmInfo());
     }
 }
